a55373a791a8ac52a1cc0b4de36140e2a5da21cd
Enes Talha Uçar <enestalha0208@gmail.com>
2023-11-20 16:55:32 +0300

    Initial Commit

---
+++ firstLectureOfSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  firstLectureOfSwiftUI
+//
+//  Created by Enes Talha Uçar  on 20.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ firstLectureOfSwiftUI/firstLectureOfSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstLectureOfSwiftUIApp.swift
+//  firstLectureOfSwiftUI
+//
+//  Created by Enes Talha Uçar  on 20.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct firstLectureOfSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
